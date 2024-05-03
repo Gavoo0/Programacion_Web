7d210a93b4a93055a52e2b554abc04d32984a057
Gavoo0 <gabrieli.godoyl@gmail.com>
2024-05-03 11:25:29 -0400

    a

---
+++ Ejercicio_prueba/assets/asset.js
@@ -0,0 +1,23 @@
+function navbar(){
+    var navbar="";
+    navbar+="<nav class='navbar navbar-expand-lg navbar-dark bg-dark'>";
+    navbar+="<div class='logo'>"
+    navbar+="<img src='../images/pngtree-travel-tourism-logo-design-template-image_390645.jpg' alt='logo' class='img-fluid'>"
+    navbar+="</div>"
+    navbar+="<button class='navbar-toggler' type='button' data-toggle='collapse' data-target='#navbarSupportedContent' aria-controls='navbarSupportedContent' aria-expanded='false' aria-label='Toggle navigation'>"
+    navbar+="<span class='navbar-toggler-icon'>"
+    navbar+="</span>"
+    navbar+="</button>"
+    navbar+="<div class='collapse navbar-collapse' id='navbarSupportedContent'>"
+    navbar+="<ul class='navbar-nav mr-auto'>"
+    navbar+="<li class='nav-item active'>"
+    navbar+="<a class='btn' href='index.html'>"
+    navbar+="INICIO"
+    navbar+="<span class='sr-only'>"
+    navbar+="(current)"
+    navbar+="</span>"
+    navbar+="</a>"
+    navbar+="</li>"
+    navbar+="<li class='nav-item'>"
+    navbar+=""
+}
